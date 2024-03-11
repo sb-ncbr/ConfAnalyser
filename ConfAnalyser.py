c528ff1e4442205115d4e8710b5b6be862461d6c
Faerlynn <faerlynnthefairy@gmail.com>
2024-03-11 16:34:23 +0100

    CA can now be imported and called directly from other python code

--- ConfAnalyser.py
+++ ConfAnalyser.py
@@ -6,58 +6,115 @@ from Utils.config import Config
 from Utils.worker import work_file
 from Utils.utils import load_file, load_names
 
-PERF_TEST = False
+PERF_TEST = True
 PARALLEL = True
 
-def run(paths_file: str, names_file: str, molecule_type: MoleculeType,
-        print_list: bool, print_summary: bool, print_all: bool) -> None:
-    """
-    Main runner function, loads all the data, creates dataset to run the
-    program on, initializes config file and then runs the program
-    either in single-thread mode or in multi-thread mode.
-    """
+class ConfAnalyser:
+    # Exists so users can use this property when calling CA without further imports
+    MoleculeType = MoleculeType
 
-    # Load data from the drive
-    files = load_file(paths_file)
-    names = load_names(names_file)
-    cfg = Config()
+    def __init__(self, paths_file: str, names_file: str,
+                 molecule_type: MoleculeType, print_list: bool = False,
+                 print_summary: bool = False, print_all: bool = True,
+                 parallel: bool = False, perf_test: bool = False) -> None:
+        """
+        Driver class for ConfAnalyser.
 
-    # Initialize the molecule structure with dictionary of atom names
-    Molecule.initialize(names)
+        This class exists to be imported by outside programs.
 
-    # Prepare the dataset to work on
-    data = [(file, names, cfg, molecule_type, PARALLEL) for file in files]
+        Example:
 
-    # Run the analysis
-    if PARALLEL:
-        with Pool() as p:
-            Molecule.molecules = list(p.map(work_file, data))
-    else:
-        for file in data:
-            Molecule.molecules.append(work_file(file))
+        from ConfAnalyser import ConfAnalyser
+        ConfAnalyser(paths_file=paths_to_pdbs_path, names_file=atom_names_path,
+                     molecule_type=ConfAnalyser.MoleculeType.Cyclohexane)
 
-    # Remove all the invalid entries due to errors
-    Molecule.molecules = [x for x in Molecule.molecules if x is not None]
 
-    # Print out results based on input parameters
-    if print_list or print_all:
-        for m in Molecule.molecules:
-            print(m)
-    if print_summary or print_all:
-        if len(Molecule.molecules) == 0:
-            print("No molecules detected!")
+        :param paths_file: a path to the file with all pdb file paths
+        :param names_file:  a path to the file with atom names
+        :param molecule_type: a molecule type of processed files
+        :param print_list: after analysis is done, print list of files and
+                           their determined conformations
+        :param print_summary: after analysis is done, print summary result
+        :param print_all:  after analysis is done, print both list and summary
+        :param parallel:   enable parallel processing of data
+        :param perf_test:  enable performance test, displaying runtime in seconds
+        """
+
+        self.paths_file = paths_file
+        self.names_file = names_file
+        self.molecule_type = molecule_type
+        self.print_list = print_list
+        self.print_summary = print_summary
+        self.print_all = print_all
+        self.parallel = parallel or PARALLEL and False  # temporarily disabled
+        self.perf_test = perf_test or PERF_TEST
+
+
+        if self.parallel:
+            from multiprocessing import Pool
+
+
+        if self.perf_test:
+            from time import perf_counter
+
+            start_time = perf_counter()
+            self.run()
+            print(f"Program finished after {perf_counter() - start_time} seconds")
+
         else:
-            if print_list or print_all:
-                print()
+            self.run()
 
-            # Call the print of statistics, the call is callable from any
-            # molecule, using 1st one is safe option.
-            Molecule.molecules[0].print_statistics()
+    def run(self) -> None:
+        """
+        Main runner function, loads all the data, creates dataset to run the
+        program on, initializes config file and then runs the program
+        either in single-thread mode or in multi-thread mode.
+        """
+
+        # Load data from the drive
+        files = load_file(self.paths_file)
+        names = load_names(self.names_file)
+        cfg = Config()
+
+        # Initialize the molecule structure with dictionary of atom names
+        Molecule.initialize(names)
+
+        # Prepare the dataset to work on
+        data = [(file, names, cfg, self.molecule_type, PARALLEL) for file in files]
+
+        # Run the analysis
+        if self.parallel:
+            with Pool() as p:
+                Molecule.molecules = list(p.map(work_file, data))
+        else:
+            for file in data:
+                Molecule.molecules.append(work_file(file))
+
+        # Remove all the invalid entries due to errors
+        Molecule.molecules = [x for x in Molecule.molecules if x is not None]
+
+        # Print out results based on input parameters
+        if self.print_list or self.print_all:
+            for m in Molecule.molecules:
+                print(m)
+        if self.print_summary or self.print_all:
+            if len(Molecule.molecules) == 0:
+                print("No molecules detected!")
+            else:
+                if self.print_list or self.print_all:
+                    print()
+
+                # Call the print of statistics, the call is callable from any
+                # molecule, using 1st one is safe option.
+                Molecule.molecules[0].print_statistics()
 
 
 def argument_parser():
     """
     Parse arguments from the command line.
+
+    This function only gets called if ConfAnalyser is called as a program
+    directly from the command line.
     """
     parser = ArgumentParser(prog="python ConfAnalyser.py")
     if os_name == "posix":
@@ -112,19 +169,9 @@ def main():
     print_summary = args.summary
     print_all = args.all or (not print_list and not print_summary)
 
-    run(paths_file, names_file, molecule_type, print_list, print_summary, print_all)
+    ConfAnalyser(paths_file, names_file, molecule_type, print_list, print_summary, print_all)
 
 
 if __name__ == "__main__":
     # Additional imports based on what is needed.
-    if PERF_TEST:
-        from time import perf_counter
-    if PARALLEL:
-        from multiprocessing import Pool
-
-    if PERF_TEST:
-        start_time = perf_counter()
-        main()
-        print(f"Program finished after {perf_counter() - start_time} seconds")
-    else:
-        main()
+    main()
