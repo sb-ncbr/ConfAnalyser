f7da1ac2a97a6a882b6a133852b5b2000824c3c0
Aliaksei Chareshneu <chareshneu.tech@gmail.com>
2024-04-04 09:45:58 +0300

    add basic logging

--- electron_density_coverage_analysis/electron_density_coverage_analysis.py
+++ electron_density_coverage_analysis/electron_density_coverage_analysis.py
@@ -1,59 +1,68 @@
 import argparse
+import logging
 import gemmi
 import statistics as st
 import math
 
 # compare intensity, corresponding to the given position, to the threshold for isosurface (MORE vs MORE OR EQUAL)
 def determine_atom_coverage(pos, map, sigma_lvl, args: argparse.Namespace):
-    if args.more_or_equal:
-        return get_intensity(pos, map, args) >= sigma_lvl
-    return get_intensity(pos, map, args) > sigma_lvl
+    try:
+        if args.more_or_equal:
+            return get_intensity(pos, map, args) >= sigma_lvl
+        return get_intensity(pos, map, args) > sigma_lvl
+    except Exception as e:
+        logging.error(e, stack_info=True, exc_info=True)
 
 # get intensity corresponding to the given position (trilinear interpolation vs itensity of the closest voxel)
 def get_intensity(pos, map, args: argparse.Namespace):
-    if args.closest_voxel:
-        return map.grid.get_nearest_point(pos).value
-    return map.grid.interpolate_value(pos)
+    try:
+        if args.closest_voxel:
+            return map.grid.get_nearest_point(pos).value
+        return map.grid.interpolate_value(pos)
+    except Exception as e:
+        logging.error(e, stack_info=True, exc_info=True)
 
 def run_analysis(args: argparse.Namespace):
-    str = gemmi.read_pdb(args.input_cycle_pdb)
-    map = gemmi.read_ccp4_map(args.input_density_ccp4)
-    # fix this part
-    # map.setup()
-    map.setup(float('nan'))
+    try:
+        str = gemmi.read_pdb(args.input_cycle_pdb)
+        map = gemmi.read_ccp4_map(args.input_density_ccp4)
+        # fix this part
+        # map.setup()
+        map.setup(float('nan'))
 
-    # calculate the sigma values
-    grid_values = []
-    for point in map.grid:
-        if not math.isnan(point.value):
-            grid_values.append(point.value)
+        # calculate the sigma values
+        grid_values = []
+        for point in map.grid:
+            if not math.isnan(point.value):
+                grid_values.append(point.value)
 
-    std = st.pstdev(grid_values)
-    sigma_lvl = 1.5 * std
+        std = st.pstdev(grid_values)
+        sigma_lvl = 1.5 * std
 
-    if args.s:
-        total_atom_count = 0
-        covered_atoms_count = 0
-        for model in str:
-            for chain in model:
-                for res in chain:
-                    for atom in res:
-                        total_atom_count = total_atom_count + 1
-                        if determine_atom_coverage(atom.pos, map, sigma_lvl, args):
-                            covered_atoms_count = covered_atoms_count + 1
+        if args.s:
+            total_atom_count = 0
+            covered_atoms_count = 0
+            for model in str:
+                for chain in model:
+                    for res in chain:
+                        for atom in res:
+                            total_atom_count = total_atom_count + 1
+                            if determine_atom_coverage(atom.pos, map, sigma_lvl, args):
+                                covered_atoms_count = covered_atoms_count + 1
 
-        print(covered_atoms_count, total_atom_count, sep=";", end='')
+            print(covered_atoms_count, total_atom_count, sep=";", end='')
 
-    if args.d:
-        for model in str:
-            for chain in model:
-                for res in chain:
-                    for atom in res:
-                        if determine_atom_coverage(atom.pos, map, sigma_lvl, args):
-                            print(atom.serial, "y", sep=";", end=';')
-                        else:
-                            print(atom.serial, "n", sep=";", end=';')
-
+        if args.d:
+            for model in str:
+                for chain in model:
+                    for res in chain:
+                        for atom in res:
+                            if determine_atom_coverage(atom.pos, map, sigma_lvl, args):
+                                print(atom.serial, "y", sep=";", end=';')
+                            else:
+                                print(atom.serial, "n", sep=";", end=';')
+    except Exception as e:
+        logging.error(e, stack_info=True, exc_info=True)
 
 def main():
     parser = argparse.ArgumentParser(description="Determines electron density coverage of a cycle. By default, uses trilinear interpolation to infer the electron density value, and considers an atom to be covered by the electron density when the corresponding intensity is MORE than the threshold for the isosurface (1.5 sigma)")
